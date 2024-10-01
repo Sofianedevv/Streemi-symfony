47b337925fbe9107c31bf5ca128ad06fee5222da
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2024-10-01 16:40:40 +0200

    premier push

---
+++ streemi/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        // Renvoie une vue Twig (HTML)
+        return $this->render('index.html.twig');
+    }
+}
