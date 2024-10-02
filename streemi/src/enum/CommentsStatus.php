4309e6e1ef1f45437475a4c7329156e877095a4f
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2024-10-02 14:03:35 +0200

    ajout de fk et de l'include

--- streemi/src/enum/CommentsStatus.php
+++ streemi/src/enum/CommentsStatus.php
@@ -7,6 +7,6 @@ namespace App\enum;
 enum CommentsStatus: string
 {
     case Pending = 'pending';      // En attente de validation
-    case Approved = 'approved';    // Approuvé et visible
+    case Publish = 'publish';    // Approuvé et visible
     case Rejected = 'rejected';    // Rejeté, non visible
 }
