f24defe7a02d0b300457ae1766f5651c6c75cbd9
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2024-12-18 20:07:55 +0100

    ajout bouton detail serie et movie

--- streemi/src/Controller/Media/Movie/MovieController.php
+++ streemi/src/Controller/Media/Movie/MovieController.php
@@ -9,14 +9,25 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Movie;
 
 class MovieController extends AbstractController
-{
-    #[Route(path: '/movie/{id}', name: 'page_detail_movie')]
-    public function detailMovie(Movie $movie): Response
+{#[Route(path: '/movie/{id}', name: 'page_detail_movie')]
+    public function detailMovie(MediaRepository $mediaRepository, int $id): Response
     {
-        return $this->render( 'media/movie/detail.html.twig', [
-            'movie' => $movie
+        $movie = $mediaRepository->find($id);
+    
+        if (!$movie) {
+            throw $this->createNotFoundException('Movie not found with ID ' . $id);
+        }
+    
+        if (!$movie instanceof Movie) {
+            throw $this->createNotFoundException('The media with ID ' . $id . ' is not a Movie.');
+        }
+    
+        return $this->render('media/movie/detail.html.twig', [
+            'movie' => $movie,
         ]);
     }
+    
+
 
     #[Route(path: '/movies', name: 'page_movies')]
     public function showAllSerie(MediaRepository $mediaRepository): Response
